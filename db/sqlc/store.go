45284ae79edf7c70f1c18f03710851b3d4dd0666
mohammadrezaataeii <mohammadrezaaataeii@gmail.com>
2024-11-30 00:55:59 +0330

    refactore AddAccountBalance logic

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -56,8 +56,6 @@ type TransferTxResult struct {
 
 var txKey = struct{}{}
 
-
-
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 	err := store.execTx(ctx, func(q *Queries) error {
@@ -94,40 +92,24 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		}
 
 		if arg.FromAccountID < arg.ToAccountID {
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				Amount: -arg.Amount,
-				ID:     arg.FromAccountID,
-			})
-			if err != nil {
-				return err
-			}
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
 
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				Amount: arg.Amount,
-				ID:     arg.ToAccountID,
-			})
-			if err != nil {
-				return err
-			}
-
-			return nil
 		} else {
-			result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				Amount: arg.Amount,
-				ID:     arg.ToAccountID,
-			})
-			if err != nil {
-				return err
-			}
-			result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
-				Amount: -arg.Amount,
-				ID:     arg.FromAccountID,
-			})
-			if err != nil {
-				return err
-			}
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
 		}
+
+		
 		return nil
 	})
 	return result, err
+}
+
+func addMoney(ctx context.Context, q *Queries, accountID1, amount1, accountID2, amount2 int64) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{Amount: amount1, ID: accountID1})
+	if err != nil {
+		return
+	}
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{Amount: amount2, ID: accountID2})
+
+	return
 }
