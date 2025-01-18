be03a762ed8f43d563abef1ab105a27465bc181a
mohammadrezaataeii <mohammadrezaaataeii@gmail.com>
2025-01-19 00:50:18 +0330

    update nullable parameters query

--- db/query/user.sql
+++ db/query/user.sql
@@ -16,3 +16,14 @@ email
 -- name: GetUser :one
 SELECT * FROM users
 WHERE username = $1 LIMIT 1;
+
+--set nullable parameters
+
+-- name: UpdateUser :one
+UPDATE users
+SET hashed_password = COALESCE(sqlc.narg(hashed_password),hashed_password),
+  full_name = COALESCE(sqlc.narg(full_name),full_name),
+  email = COALESCE(sqlc.narg(email),email),
+  username = sqlc.arg(username)
+  RETURNING *;
+
