5c6c31a9edef33c8755cf14bc435914c5f80b11a
mohammadrezaataeii <mohammadrezaaataeii@gmail.com>
2025-01-21 00:22:02 +0330

    password changed at

--- db/query/user.sql
+++ db/query/user.sql
@@ -21,7 +21,9 @@ WHERE username = $1 LIMIT 1;
 
 -- name: UpdateUser :one
 UPDATE users
-SET hashed_password = COALESCE(sqlc.narg(hashed_password),hashed_password),
+SET
+  hashed_password = COALESCE(sqlc.narg(hashed_password),hashed_password),
+  password_changed_at = COALESCE(sqlc.narg(password_changed_at),password_changed_at),
   full_name = COALESCE(sqlc.narg(full_name),full_name),
   email = COALESCE(sqlc.narg(email),email),
   username = sqlc.arg(username)
